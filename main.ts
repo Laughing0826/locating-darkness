180c9f7953d4f10d2bcd507c406fbe03ddf4222b
Laughing0826 <155951133+Laughing0826@users.noreply.github.com>
2024-01-08 15:53:00 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    MaxSoundLevel = 0
+    basic.showIcon(IconNames.Happy)
+    for (let index = 0; index < 100; index++) {
+        MaxSoundLevel = Math.max(MaxSoundLevel, Environment.ReadNoise(AnalogPin.P0))
+        basic.pause(100)
+    }
+    result = MaxSoundLevel
+    basic.showNumber(result)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(result)
+})
+let MaxSoundLevel = 0
+let result = 0
+basic.showIcon(IconNames.Surprised)
+result = 0
